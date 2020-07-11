395956dcf530d191e38fba5a095373c3d7dacfcf
Gilberto Aviles <gilbertoaviles@MacBook-Pro-de-Gilberto.local>
2020-07-11 16:07:51 -0500

    Se agrega directorio para trabajar con 'Bulma'.Primeras configuraciones de Bulma.

---
+++ sesion04/getting-started-with-bulma/src/index.js
@@ -0,0 +1,6 @@
+import './sass/mybulma.scss'
+import './sass/style.scss'
+import '@fortawesome/fontawesome-free/js/all'
+
+const sayHello = 'Hello World with Bulma'
+console.log(sayHello)
---
+++ sesion04/getting-started-with-webpack/src/index.js
@@ -0,0 +1,7 @@
+// require('@fortawesome/fontawesome-free/js/all')
+// require('./sass/style.scss')
+import './sass/style.scss'
+import '@fortawesome/fontawesome-free/js/all'
+
+const sayHello = 'Hello World with Webpack'
+console.log(sayHello)
