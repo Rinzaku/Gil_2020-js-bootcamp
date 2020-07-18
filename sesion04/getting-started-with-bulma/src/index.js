f6d143c141fc5b6625a4680ec1c8cb83f76a2712
Gilberto Aviles <gilbertoaviles@MacBook-Pro-de-Gilberto.local>
2020-07-18 16:07:34 -0500

    Se termina de ver la introducción a Bulma. Se Incia a ver la información de los elementos de HTML con Bulma.

--- sesion04/getting-started-with-bulma/src/index.js
+++ sesion04/getting-started-with-bulma/src/index.js
@@ -1,4 +1,5 @@
-import './sass/mybulma.scss'
+// import 'bulma/bulma.sass'  //do not do this, you need to modify bulma's variables
+import './sass/mybulma.scss' //remomended way
 import './sass/style.scss'
 import '@fortawesome/fontawesome-free/js/all'
 
---
+++ sesion05/bulma-playground/src/index.js
@@ -0,0 +1,6 @@
+import './sass/mybulma.scss'
+import './sass/style.scss'
+import '@fortawesome/fontawesome-free/js/all'
+
+const sayHello = 'Bulma PlayGround'
+console.log(sayHello)
